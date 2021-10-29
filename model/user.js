918e57bebb66aaa182baf43252442af16d25d7bc
SH <https://github.com/miracleSH>
2021-10-30 00:40:12 +0900

    로그인 기능 구현

--- model/user.js
+++ model/user.js
@@ -1,56 +1,82 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 const userSchema = mongoose.Schema({
-    name : {
-        type: String,
-        maxlength : 50
-    },
-    email : {
-        type : String,
-        trim : true,
-        unique : 1
-    },
-    password : {
-        type: String,
-        minglength : 5
-    },
-    lastname : {
-        type:String,
-        maxlength : 50
-    },
-    role : {
-        type : Number,
-        default : 0
-    },
-    token : {
-        type : String
-    },
-    tokenExp : {
-        type : Number
-    },
-    image : {
-        type : String
-    }
-    
-})
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minglength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+  image: {
+    type: String,
+  },
+});
 
-userSchema.pre('save', function(next){
-    //비밀번호 암호화
-    let user = this;
-    if(user.isModified('password')){
-        bcrypt.genSalt(saltRounds, (err, salt)=>{
-            if(err) return next(err);
-            bcrypt.hash(user.password, salt, (err, hash)=>{
-                if(err) return next(err);
-                user.password = hash
-                next()
-            })
-        })
-    }
-})
+userSchema.pre("save", function (next) {
+  //비밀번호 암호화
+  var user = this;
 
-const User = mongoose.model('User', userSchema);
+  //비밀번호가 수정될 때만 암호화
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
-module.exports = {User}
+userSchema.methods.comparePassword = (
+  plainPassword,
+  encodedPassword,
+  callback
+) => {
+  //plainPassword 와 암호화된 비밀번호랑 같은지 확인
+  bcrypt.compare(plainPassword, encodedPassword, (err, isMatch) => {
+    console.log(encodedPassword);
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
 
+userSchema.methods.generateToken = (user, callback) => {
+  //jsonwebtoken을 사용해서 토큰생성
+  console.log(user);
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  user.token = token;
+  user.save((err, user) => {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+};
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
