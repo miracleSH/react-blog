c047a5f3022a475a9e1a958388876e33df77b364
SH <https://github.com/miracleSH>
2021-12-14 23:43:00 +0900

    Revert "초기 세팅"
    This reverts commit 07dcba165616c98db52041896527305a2ada607b.

--- README.md
+++
@@ -1,9 +0,0 @@
-# react-blog
-
-## 2021-10-29
-
-## DB : mongoDB
-
-## front : React
-
-## back : node
--- server/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI : "mongodb+srv://seungheon:dustn@@56@react-blog.njxco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-}
